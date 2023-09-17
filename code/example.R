6316654f80afc68ba9afc6552c9a7b81995112bf
Michael Klesel <michael@klesel.info>
2023-09-17 12:29:18 +0200

    test debugger renv

--- code/example.R
+++ code/example.R
@@ -1,23 +1,28 @@
-# install.packages("renv")
-library(renv)
-# renv::init()
-# renv::install("languageserver")
-# renv::install("/code/renv/cellar/vscDebugger_0.5.2.tar.gz")
-library(languageserver)
-library(here)
-library(vscDebugger)
-
-# Make sure renv is deactivated
+# Makue sure renv is deactivated
 # renv::deactivate()
+
+# Install vscDebugger locally
+# devtools::install_local("/code/renv/cellar/vscDebugger_0.5.2.tar.gz")
+
+library(languageserver)
+library(vscDebugger)
 
 # In this case, the debugger works perfect
 addNumbers <- function(x,y){
     z = x + y
     return(z)
 }
-
 print(addNumbers(2,3))
 
 
 # Activate renv
-renv::activate()
+# renv::activate()
+# renv::install(c("languageserver"))
+# renv::install("/code/renv/cellar/vscDebugger_0.5.2.tar.gz")
+
+# In this case, the debugger works perfect
+addNumbers <- function(x,y){
+    z = x + y
+    return(z)
+}
+print(addNumbers(2,3))
--- dockerfile
+++ dockerfile
@@ -1,6 +1,16 @@
 FROM rocker/tidyverse:4.2.3
 
+USER root
+
 RUN mkdir /code
 WORKDIR /code
 
-CMD [ "/bin/bash" ]
+# R set Posit Manager to fasten things up
+# https://www.r-bloggers.com/2023/07/posit-package-manager-for-linux-r-binaries/
+RUN echo "options(repos = c(CRAN = 'https://packagemanager.posit.co/cran/__linux__/jammy/latest'))" >> /etc/R/Rprofile.site
+RUN echo 'options(HTTPUserAgent = sprintf("R/%s R (%s)", getRversion(), paste(getRversion(), R.version["platform"], R.version["arch"], R.version["os"])))'  >> /etc/R/Rprofile.site
+# Install R dependencies
+ENV RENV_VERSION 1.0.2
+RUN R -e "install.packages(c('languageserver', 'jsonlite', 'devtools', 'httpgd', 'renv'))"
+
+CMD ["/bin/bash"]
